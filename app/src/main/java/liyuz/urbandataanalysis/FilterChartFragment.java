c8aefa6ac3076407115b9541b9c757c090500eb6
LiamZ <liamz95@outlook.com>
2018-01-07 22:08:12 +1100

    a working alert diaglog

--- app/src/main/java/liyuz/urbandataanalysis/FilterChartFragment.java
+++ app/src/main/java/liyuz/urbandataanalysis/FilterChartFragment.java
@@ -4,8 +4,11 @@ package liyuz.urbandataanalysis;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -31,7 +34,7 @@ import java.net.URL;
 import java.util.ArrayList;
 
 
-public class FilterChartFragment extends Fragment implements View.OnClickListener{
+public class FilterChartFragment extends Fragment {
 
 //    private Button areaBtn;
     private Button areaBtn;
@@ -40,7 +43,6 @@ public class FilterChartFragment extends Fragment implements View.OnClickListene
     private Button lvlBtn;
     private Button colorBtn;
 
-    private AlertDialog.Builder builder;
     private ProgressDialog progressDialog;
 
     private ArrayList<String> attributes = new ArrayList<>();
@@ -54,9 +56,21 @@ public class FilterChartFragment extends Fragment implements View.OnClickListene
     private String seletedLevel;
     private String selectedColor;
 
+    public static final int SHOW_RESPONSE = 0;
+
     private int checkedItem = 0;
 
     private String TAG = getClass().getSimpleName() + "### ";
+
+//    private Handler chartFragmentHandler = new Handler(){
+//        public void handleMessage(Message msg){
+//            switch (msg.what) {
+//                case SHOW_RESPONSE:
+//                    String response = (String) msg.obj;
+//                    parseXML(response);
+//            }
+//        }
+//    };
 
     public FilterChartFragment() {
         // Required empty public constructor
@@ -68,10 +82,7 @@ public class FilterChartFragment extends Fragment implements View.OnClickListene
         // Inflate the layout for this fragment
         View mView = inflater.inflate(R.layout.fragment_filter_chart, container, false);
 
-        // Show progress dialog
-        FilterProgressOperation myTask = null;
-        myTask = new FilterProgressOperation();
-        myTask.execute();
+        openLocalFile();
 
         areaBtn = (Button) mView.findViewById(R.id.filter_chart_area_btn);
         attrBtn = (Button) mView.findViewById(R.id.filter_chart_attribute_btn);
@@ -79,30 +90,53 @@ public class FilterChartFragment extends Fragment implements View.OnClickListene
         lvlBtn = (Button) mView.findViewById(R.id.filter_chart_level_btn);
         colorBtn = (Button) mView.findViewById(R.id.filter_chart_color_btn);
 
-        areaBtn.setOnClickListener(this);
-        attrBtn.setOnClickListener(this);
-        clasBtn.setOnClickListener(this);
-        lvlBtn.setOnClickListener(this);
-        colorBtn.setOnClickListener(this);
-//        // Setting the pop up window for attributes selection
-//        builder = new AlertDialog.Builder(getActivity());
-//
-//        final String[] animals = {"horse", "cow", "camel", "sheep", "goat"};
-//
-//        attrBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                 builder.setTitle("Select an attribute");
-//                 builder.setSingleChoiceItems(animals, checkedItem, new DialogInterface.OnClickListener() {
-//                     @Override
-//                     public void onClick(DialogInterface dialogInterface, int i) {
-//                         Toast.makeText(getActivity(), "Selected an attribute", Toast.LENGTH_SHORT).show();
-//                         selectedAttribute = animals[i];
-//                     }
-//                 });
-//
-//            }
-//        });
+//        areaBtn.setOnClickListener(this);
+//        attrBtn.setOnClickListener(this);
+//        clasBtn.setOnClickListener(this);
+//        lvlBtn.setOnClickListener(this);
+//        colorBtn.setOnClickListener(this);
+
+        // Get data
+
+
+        // Show progress dialog
+//        FilterProgressOperation myTask = null;
+//        myTask = new FilterProgressOperation();
+//        myTask.execute();
+
+        // Setting the pop up window for attributes selection
+
+        final String[] animals = {"horse", "cow", "camel", "sheep", "goat"};
+
+        attrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Select an attribute");
+
+                builder.setSingleChoiceItems(animals, checkedItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getActivity(), "Selected an attribute", Toast.LENGTH_SHORT).show();
+                        selectedAttribute = animals[i];
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Handles actions to perform when user confirm their operations
+
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", null);
+
+                // Create and show alert dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 //
 //        clasBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -121,29 +155,30 @@ public class FilterChartFragment extends Fragment implements View.OnClickListene
         return  mView;
     }
 
-    @Override
-    public void onClick(View view) {
-
-        builder = new AlertDialog.Builder(getActivity());
-
-        switch (view.getId()) {
-            case R.id.filter_chart_area_btn:
-                Toast.makeText(getActivity(), "Area button clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.filter_chart_attribute_btn:
-                Toast.makeText(getActivity(), "Attribute button clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.filter_chart_classifier_btn:
-                Toast.makeText(getActivity(), "Classifier button clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.filter_chart_color_btn:
-                Toast.makeText(getActivity(), "Color button clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.filter_chart_level_btn:
-                Toast.makeText(getActivity(), "Level button clicked", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
+//    @Override
+//    public void onClick(View view) {
+//
+//        builder = new AlertDialog.Builder(getActivity());
+//
+//        switch (view.getId()) {
+//            case R.id.filter_chart_area_btn:
+//                Toast.makeText(getActivity(), "Area button clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.filter_chart_attribute_btn:
+//                Toast.makeText(getActivity(), "Attribute button clicked", Toast.LENGTH_SHORT).show();
+////                builder.setTitle("Choose an attribute");
+//                break;
+//            case R.id.filter_chart_classifier_btn:
+//                Toast.makeText(getActivity(), "Classifier button clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.filter_chart_color_btn:
+//                Toast.makeText(getActivity(), "Color button clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.filter_chart_level_btn:
+//                Toast.makeText(getActivity(), "Level button clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//    }
 
 
     private class FilterProgressOperation extends AsyncTask<String, Void, String> {
@@ -203,7 +238,12 @@ public class FilterChartFragment extends Fragment implements View.OnClickListene
                         response.append(line);
                     }
                     String data = response.toString();
-                    parseXML(data);
+//                    parseXML(data);
+
+//                    Message message = new Message();
+//                    message.what = SHOW_RESPONSE;
+//                    message.obj = data;
+//                    chartFragmentHandler.sendMessage(message);
 
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
@@ -275,6 +315,27 @@ public class FilterChartFragment extends Fragment implements View.OnClickListene
             }
         } catch (XmlPullParserException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openLocalFile() {
+        AssetManager assetManager = getActivity().getApplicationContext().getAssets();
+        try{
+            InputStream in = assetManager
+                    .open("aurin-datasource-SA_Govt_RenewalSA-UA_WISeR_renewalsa_age_household_ref_person_2011.xml");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+
+            String data = stringBuilder.toString();
+            Log.d(TAG, "All data read from local xml file");
+            parseXML(data);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
